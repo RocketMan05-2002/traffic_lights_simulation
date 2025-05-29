4546c45def697fad6d0fcd2a0b8681ea164edd5f
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-29 18:53:25 +0530

    double digit simulation implemented successfully

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import Cell from './components/Cell';
 import './styles.css'
 
 const digitMap = {
-  1: [2, 5, 8],
-  2: [1, 2, 4, 5, 7, 8],
-  3: [0, 1, 2, 4, 5, 6, 7, 8],
-  4: [0, 2, 4, 5, 8],
-  5: [0, 1, 2, 3, 4, 6, 7],
-  6: [0, 3, 4, 5, 6, 7, 8],
-  7: [0, 1, 2, 5, 8],
-  8: [0, 1, 2, 4, 6, 7, 8],
-  9: [0, 1, 2, 3, 5, 7, 8],
-};
+    0: [0, 1, 2, 3, 5, 6, 7, 8],
+    1: [2, 5, 8],
+    2: [1, 2, 4, 5, 7, 8],
+    3: [0, 1, 2, 4, 5, 6, 7, 8],
+    4: [0, 2, 4, 5, 8],
+    5: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    6: [0, 3, 4, 5, 6, 7, 8],
+    7: [0, 1, 2, 5, 8],
+    8: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    9: [0, 1, 2, 3, 4, 5, 6, 8]
+}
 
 const App = () => {
-
-    const[order,setOrder] = useState([]);
+    const[leftOrder, setLeftOrder] = useState([]);
+    const[rightOrder, setRightOrder] = useState([]);
     const[input,setInput] = useState(null);
-    const[showInput, setShowInput] = useState(null);
+    const [showInput,setShowInput] = useState(null);
 
     const config = [
         [1,1,1],
@@ -28,45 +29,66 @@ const App = () => {
 
     function onAddValue(e){
         if(e.keyCode === 13 && e.target.value){
-            setInput(Number(e.target.value));
+            setInput(e.target.value);
         }
     }
 
     useEffect(()=>{
-        if(!input && input<1 && input>10) return;
+        if(!input || input<1 || input>99) return;
         let current = input;
+
         const timer = setInterval(()=>{
-            setOrder(digitMap[current] || []);
+            const left  = Math.floor(current/10);
+            const right = current % 10;
+
+            setLeftOrder(digitMap[left]|| []);
+            setRightOrder(digitMap[right]||[]);
             setShowInput(current);
+
             current--;
             if(current<1){
                 clearInterval(timer);
                 setTimeout(()=>{
-                    setOrder([]);
+                    setLeftOrder([]);
+                    setRightOrder([]);
                     setShowInput(0);
-                },2000);
+                },1000);
             }
-        },2000);
+        },1000);
         return ()=>clearInterval(timer);
-    },[input])
+
+    },[input]);
+
 
   return (
     <div className='wrapper'>
-        <h1 style={{color:"red" , textTransform: "uppercase" }}>Traffic lights simulation</h1>
-        <input autoFocus type='number' min={1} max={9} placeholder='enter the value to countdown' onKeyDown={onAddValue} />
-        <div className='grid' style={{ gridTemplateColumns: `repeat(${config[0].length},1fr)` }}>
-            {
-                config.flat(1).map((_,ind)=>{
-                    return <Cell 
-                    key={ind}
-                    filled={order.includes(ind)}
-                    />
-                })
-            }
+        <h1 style={{ color: "red", textTransform: "uppercase" }}>Traffic lights simulator</h1>
+        <input type='number' placeholder='enter number to coundown' min={1} max={99} onKeyDown={onAddValue} />
+        <div className='grid-box' style={{ display: "flex",  gap:"20px" }}>
+            <div className='grid' style={{ gridTemplateColumns: `repeat(3,1fr)` }}>
+                {
+                    config.flat(1).map((_,ind)=>(
+                        <Cell 
+                        key={ind}
+                        filled={leftOrder.includes(ind)}
+                        />
+                    ))
+                }
+            </div>
+            <div className='grid' style={{ gridTemplateColumns: `repeat(3,1fr)` }} >
+                {
+                    config.flat(1).map((_,ind)=>(
+                        <Cell
+                        key={ind}
+                        filled={rightOrder.includes(ind)}
+                        />
+                    ))
+                }
+            </div>
         </div>
-        <h1 style={{ color:"red" }}>{showInput}</h1>
+        <h1 style={{ color: "red" }}>{showInput}</h1>
     </div>
   )
 }
 
-export default App;
+export default App
